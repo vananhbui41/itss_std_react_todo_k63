e6edac1f62134a4641e40a8f645ac405c1c36448
vananh <vananhbui41@gmail.com>
2022-06-06 00:58:32 +0700

    Save Todo

--- src/hooks/storage.js
+++ src/hooks/storage.js
@@ -13,18 +13,26 @@ const STORAGE_KEY = 'itss-todo';
 
 function useStorage() {
   const [items, setItems] = useState([]);
-　
-　/* 副作用を使う */
+
+  /* 副作用を使う */
   useEffect(() => {
-    
+    const data = localStorage.getItem(STORAGE_KEY);
+
+    if (!data) {
+      localStorage.setItem(STORAGE_KEY, JSON.stringify([]));
+    } else {
+      setItems(JSON.parse(data));
+    }
   }, []);
 
   const putItems = items => {
-    
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(items));
+    setItems  (items);
   };
 
   const clearItems = () => {
-    
+    localStorage.setItem(STORAGE_KEY, JSON.stringify([]));
+    setItems([]);
   };
 
   return [items, putItems, clearItems];
